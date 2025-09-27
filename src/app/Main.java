8a6894608d25b13506b81114faf97090c1938a81
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-09-27 12:35:01 +0300

    add converting miles to kms

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,25 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("App for converting miles to kilometers");
+
+         String title;
+         Scanner scanner;
+
+        scanner = new Scanner(System.in);
+
+        title = "Enter number of miles: ";
+        System.out.print(title);
+        double miles = scanner.nextDouble();
+
+        double kilometers = miles * 1.609344;
+        System.out.println(miles + " miles is " + kilometers + " kilometers");
+
+        scanner.close();
+
     }
 }
