d3f4cf52ea28076906ae3f6e1cbd3126fbc16c73
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-09-27 13:34:18 +0300

    adA converting mÑkmso k miles.  Ad ыуcontert selection menu

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,14 +12,35 @@ public class Main {
 
         scanner = new Scanner(System.in);
 
-        title = "Enter number of miles: ";
-        System.out.print(title);
-        double miles = scanner.nextDouble();
+        title = "Choose the type of conversion: \n1) Miles to Kilometers \n2) Kilometers to Miles";
+        byte type;
+        double distance;
+        double convertedDistance;
 
-        double kilometers = miles * 1.609344;
-        System.out.println(miles + " miles is " + kilometers + " kilometers");
+        do {
+            System.out.println(title);
+            type = scanner.nextByte();
+            if (type < 1 || type > 2) {
+                System.out.println("Invalid input, please try again");
+            }
+        } while (type < 1 || type > 2);
 
+        switch (type) {
+            case 1: {
+                System.out.print("Enter number of miles: ");
+                distance = scanner.nextDouble();
+                convertedDistance = distance * 1.609344;
+                System.out.println(distance + " miles is: " + convertedDistance + " kilometers");
+                break;
+            }
+            case 2: {
+                System.out.print( "Enter number of kilometers: ");
+                distance = scanner.nextDouble();
+                convertedDistance = distance / 1.609344;
+                System.out.println(distance + " kilometers is: " + convertedDistance + " miles");
+                break;
+            }
+        };
         scanner.close();
-
     }
 }
