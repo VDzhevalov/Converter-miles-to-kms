648de341da48620f6cc32cbe311fbe21123621b1
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-09-29 21:08:16 +0300

    refactoring: move menu functionality to method, improve converters methods

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,55 +4,56 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
-    private static double distance;
-    private static double convertedDistance;
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final double KONVERT_K = 1.609344;
 
     public static void main(String[] args) {
         System.out.println("App for converting miles to kilometers");
         String title = "Choose the type of conversion: \n1) Miles to Kilometers \n2) Kilometers to Miles";
+        double distance;
 
+        switch (selectConvertOptionMenu(title)) {
+            case 1: {
+                distance = inputSelectedDistance("Enter number of miles: ");
+                System.out.printf("%.2f miles is: %.2f kilometers", distance, convertMilesToKilometers(distance));
+                break;
+            }
+            case 2: {
+                distance = inputSelectedDistance("Enter number of kilometers: ");
+                System.out.printf("%.2f kilometers is: %.2f miles", distance, convertKilometersToMiles(distance));
+                break;
+            }
+        }
+        SCANNER.close();
+    }
+
+    private static byte selectConvertOptionMenu(String title){
         byte type;
-
         do {
             System.out.println(title);
-
             try {
-                type = scanner.nextByte();
+                type = SCANNER.nextByte();
             } catch (InputMismatchException e) {
                 type = 0;
-                scanner.nextLine();
+                SCANNER.nextLine();
             }
             if (type < 1 || type > 2) {
                 System.out.println("Invalid input, please try again");
             }
-
         } while (type < 1 || type > 2);
-
-        switch (type) {
-            case 1: {
-                convertMilesToKilometers();
-                break;
-            }
-            case 2: {
-                convertKilometersToMiles();
-                break;
-            }
-        };
-        scanner.close();
+        return type;
     }
 
-    private static void convertMilesToKilometers() {
-        System.out.print("Enter number of miles: ");
-        distance = scanner.nextDouble();
-        convertedDistance = distance * 1.609344;
-        System.out.printf("%.2f miles is: %.2f kilometers", distance, convertedDistance);
+    private static double inputSelectedDistance(String title) {
+        System.out.println(title);
+        return SCANNER.nextDouble();
     }
 
-    private static void convertKilometersToMiles() {
-        System.out.print( "Enter number of kilometers: ");
-        distance = scanner.nextDouble();
-        convertedDistance = distance / 1.609344;
-        System.out.printf("%.2f kilometers is: %.2f miles", distance, convertedDistance);
+    private static double convertMilesToKilometers(double distance) {
+        return distance * KONVERT_K;
+    }
+
+    private static double convertKilometersToMiles(double distance) {
+        return distance / KONVERT_K;
     }
 }
